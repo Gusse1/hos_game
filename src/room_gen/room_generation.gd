9d51039b24fea46a8320855350b030cc860adeaf
Miksu G <gustafssonmik4el@gmail.com>
2024-03-23 20:27:54 +0200

    Room spawning in multiple directions

--- src/room_gen/room_generation.gd
+++ src/room_gen/room_generation.gd
@@ -9,6 +9,8 @@ var spawnedRooms = []
 
 @export var rooms_to_generate: int
 
+var directions = ["north", "east", "south", "west"]
+
 func _ready():
 	_generate_rooms()
 	
@@ -26,17 +28,51 @@ func _generate_rooms():
 		spawnedRooms[room_num]._generate_offset()
 				
 		if room_num > 0:
-			var prev_room = spawnedRooms[room_num-1]
-			var curr_room = spawnedRooms[room_num]
-			print_debug("Moving new room... with room_num: ", room_num)
-			print_debug("North door position: ", prev_room.position.x + prev_room.north_door.position.x)
-			var northDoorLocation = prev_room.position + prev_room.north_door.position
-			var newPosition = northDoorLocation.x + curr_room.north_door_offset
+			# Choose a random direction:
+			var randomDirIndex = randi() % directions.size()
+			var randomDir = directions[randomDirIndex]
 			
-			print_debug(newPosition)
+			match randomDir:
+				"north":
+					_spawn_north_room(room_num)
+				"east":
+					_spawn_east_room(room_num)
+				"south":
+					_spawn_north_room(room_num)
+				"west":
+					_spawn_west_room(room_num)
+	
+func _spawn_north_room(room_num: int):
+	var prev_room = spawnedRooms[room_num-1]
+	var curr_room = spawnedRooms[room_num]
+	var northDoorLocation = prev_room.position + prev_room.north_door.position
+	var newPosition = northDoorLocation.x + curr_room.north_door_offset
+	
+	spawnedRooms[room_num].position = Vector3(newPosition, prev_room.position.y, prev_room.position.z)
 
-			spawnedRooms[room_num].position = Vector3(newPosition, 0, 0)
-			
-		print_debug("NewRoomPos: ", randomRoom.position)	
+func _spawn_east_room(room_num: int):
+	var prev_room = spawnedRooms[room_num-1]
+	var curr_room = spawnedRooms[room_num]
+	var eastDoorLocation = prev_room.position + prev_room.east_door.position
+	var newPosition = eastDoorLocation.z + curr_room.east_door_offset
 	
+	print("prev", prev_room.position)
+
+	spawnedRooms[room_num].position = Vector3(prev_room.position.x, prev_room.position.y, newPosition)
+	print("curr", curr_room.position)
+
+func _spawn_south_room(room_num: int):
+	var prev_room = spawnedRooms[room_num-1]
+	var curr_room = spawnedRooms[room_num]
+	var southDoorLocation = prev_room.position + prev_room.south_door.position
+	var newPosition = southDoorLocation.x + curr_room.south_door_offset
 	
+	spawnedRooms[room_num].position = Vector3(newPosition, prev_room.position.y, prev_room.position.z)
+	
+func _spawn_west_room(room_num: int):
+	var prev_room = spawnedRooms[room_num-1]
+	var curr_room = spawnedRooms[room_num]
+	var westDoorLocation = prev_room.position + prev_room.west_door.position
+	var newPosition = westDoorLocation.z + curr_room.west_door_offset
+	
+	spawnedRooms[room_num].position = Vector3(prev_room.position.x, prev_room.position.y, newPosition)
--- src/room_gen/room_info.gd
+++ src/room_gen/room_info.gd
@@ -10,6 +10,18 @@ var east_door_offset: float
 @export var south_door: Node3D
 @export var east_door: Node3D
 
+var north_blocked: bool
+var west_blocked: bool
+var south_blocked: bool
+var east_blocked: bool
+
 func _generate_offset():
-	north_door_offset = north_door.position.x
+	if north_door:
+		north_door_offset = north_door.position.x
+	if west_door:
+		west_door_offset = west_door.position.z
+	if south_door:
+		south_door_offset = south_door.position.x
+	if east_door:
+		east_door_offset = east_door.position.z
 
