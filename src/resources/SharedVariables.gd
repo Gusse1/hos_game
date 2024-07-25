05a57c4869aa02a359bea5792bc17369de5621f2
Miksu G <gustafssonmik4el@gmail.com>
2024-07-25 20:30:54 +0300

    Project refactor

--- src/resources/SharedVariables.gd
+++ src/resources/SharedVariables.gd
@@ -1,5 +1,10 @@
 extends Node
 
+@export_category("Player")
 @export var player_node : Node3D
 @export var player_body : Node3D
 @export var player_state : PlayerState
+
+@export_category("Video")
+@export var video_screen_player : SubViewport
+@export var eye_screen_player : SubViewport
